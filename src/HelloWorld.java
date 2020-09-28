4b94dc1cdf6d9976f08ce5ab4d4d16c92c9c2575
Daniel Fryar <daniel.fryar@gmail.com>
2020-09-28 13:57:11 -0500

    Finished Intro to Java lesson and exercises

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello, Bio!");
+        System.out.print("Hello,");
+        System.out.print(" Bio!");
+//        System.out.println();
+    }
+}
