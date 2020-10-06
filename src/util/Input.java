5a1bc1b96b7c12ac166773d3f025f9ac527f9745
Daniel Fryar <daniel.fryar@gmail.com>
2020-10-06 15:56:57 -0500

    feat: completed bonus on Input class to include prompt

--- src/util/Input.java
+++ src/util/Input.java
@@ -12,9 +12,16 @@ public class Input {
     public String getString(){
         return this.scanner.nextLine();
     }
+    public String getString(String prompt){
+        System.out.println(prompt);
+        return getString();
+    }
 
     public boolean yesNo(){
-        System.out.println("Please enter yes or no");
+        return yesNo("Please enter yes or no");
+    }
+    public boolean yesNo(String prompt){
+        System.out.println(prompt);
         String input = this.scanner.nextLine();
         return (input.trim().toLowerCase().equals("y") ||
                 input.trim().toLowerCase().equals("yes"));
@@ -22,8 +29,13 @@ public class Input {
 
 
     public int getInt(int min,int max){
+
+        return getInt(min,max,"Please enter an integer between "+min+" and "+max);
+
+    }
+    public int getInt(int min,int max,String prompt){
         while (true) {
-            System.out.printf("Please enter an integer between %d and %d", min, max);
+            System.out.println(prompt);
 
             String input = scanner.nextLine();
             int num = Integer.parseInt(input);
@@ -31,8 +43,12 @@ public class Input {
                 return num;
         }
     }
+
     public int getInt(){
-        System.out.println("Please enter an integer");
+        return getInt("Please enter an integer");
+    }
+    public int getInt(String prompt){
+        System.out.println(prompt);
         String input = scanner.nextLine();
         int num = Integer.parseInt(input);
         return num;
