624efb33ba922b27b6469f15dd159031142d2072
Daniel Fryar <daniel.fryar@gmail.com>
2020-10-06 12:22:26 -0500

    feat:finished first #1

---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String name;
+
+    public String getName(){
+//TODO: return the person's name
+        return this.name;
+    }
+
+    public void setName(String name){
+//TODO: change the name property to the passed value
+        this.name = name;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.printf("Hello, my name is %s.",this.name);
+    }
+}
