f73760f9d043b97b3bbc9ec9504ba36067346974
Fer <fernandoza@gmail.com>
2020-10-07 09:53:41 -0500

    feat: create some Arrays class examples

--- src/ArraysLec.java
+++ src/ArraysLec.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 public class ArraysLec {
 
     public static void main(String[] args) {
 
         String[] names = {"Fer", "Douglas", "Daniel"};
         double[] prices = new double[3];
+        int[] numbers = new int[8];
+        int[] otherNumbers = new int[8];
 
         prices[0] = Double.parseDouble("1.0");
         prices[1] = 2.0 * 2;
@@ -28,6 +32,26 @@ public class ArraysLec {
         for (String name: names) {
             System.out.println("name = " + name);
         }
+
+        Arrays.sort(prices);
+        Arrays.sort(names);
+
+        System.out.println("Arrays.names = " + Arrays.toString(names));
+        System.out.println("Arrays.prices = " + Arrays.toString(prices));
+
+        Arrays.fill(numbers, 10);
+        Arrays.fill(otherNumbers, 5);
+
+        System.out.println("Arrays.equals(otherNumbers, otherNumbers) = " + Arrays.equals(otherNumbers, otherNumbers));
+        System.out.println("Arrays.equals(numbers, otherNumbers) = " + Arrays.equals(numbers, otherNumbers));
+        System.out.println("Arrays.toString() = " + Arrays.toString(numbers));
+
+//        String[] copyOfNames = new String[names.length+1]; // you have to bring each item manually
+        String[] copyOfNames = Arrays.copyOf(names, names.length+1);
+
+        System.out.println("copyOfNames = " + copyOfNames.length);
+        copyOfNames[3] = "Justin";
+        System.out.println("Arrays.toString(copyOfNames) = " + Arrays.toString(copyOfNames));
 
     }
 }
