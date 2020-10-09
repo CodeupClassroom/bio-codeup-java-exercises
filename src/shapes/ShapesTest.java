198802c534b98060f8f01f70ac0759a97a5852e7
Fer <fernandoza@gmail.com>
2020-10-09 09:29:16 -0500

    feat: create a  Square class and some testing

---
+++ src/shapes/ShapesTest.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class ShapesTest {
+    public static void main(String[] args) {
+
+        Rectangle[] group = new Rectangle[2];
+
+        group[0] = new Rectangle(4, 5);
+        // polymorph
+        group[1] = new Square(5);
+
+        for (Rectangle rect : group) {
+            System.out.println("rect.getArea() = " + rect.getArea());
+            System.out.println("rect.getPerimeter() = " + rect.getPerimeter());
+        }
+
+    }
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,9 @@
+package shapes;
+
+public class Square extends Rectangle {
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+}
