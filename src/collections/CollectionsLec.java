c69b3ce374bac80bb0c1a1ea4148289b1d6efea7
Fer <fernandoza@gmail.com>
2020-10-13 10:04:19 -0500

    feat: create not simple examples of Arraylists with Clases and Objects

--- src/collections/CollectionsLec.java
+++ src/collections/CollectionsLec.java
@@ -1,15 +1,23 @@
 package collections;
 
+import oopLecture.Post;
+
 import java.util.ArrayList;
 
 public class CollectionsLec {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>();
+        ArrayList<Post> posts = new ArrayList<>();
+
         System.out.println(names.size());
         names.add("Fernando");
         System.out.println(names.size());
         names.add("Daniel");
         names.add("Kenneth");
+
+        posts.add(new Post("Fer's weekend", "Fer"));
+        posts.add(new Post("Fer's 2nd weekend", "Ana"));
+        posts.add(new Post("Ryan's latest weekend", "Ryan"));
 
         System.out.println("names.get(0) = " + names.get(0));
 //        System.out.println("names.get(1) = " + names.get(1));
@@ -18,6 +26,22 @@ public class CollectionsLec {
             System.out.println("names.get(2) = " + names.get(names.indexOf("Fernando")).toUpperCase());
         }
 
+        Post deletedPost = posts.remove(1);
+        deletedPost.printTitleAndAuthor();
+        System.out.println("posts.lastIndexOf(deletedPost) = " + posts.lastIndexOf(deletedPost));
+
+        if(!posts.isEmpty()){
+            for (Post post: posts) {
+                post.printTitleAndAuthor();
+            }
+        }
 
     }
+
+    // just for teaching purposes
+//    public static ArrayList<Post> myOwnAdd(String title, ArrayList<String> authors){
+//        ArrayList<Post> posts = new ArrayList<>();
+//        posts.add(new Post(title, authors));
+//        return posts;
+//    }
 }
