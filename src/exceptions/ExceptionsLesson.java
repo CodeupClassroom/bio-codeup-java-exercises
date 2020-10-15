5e5f6fb0fa65f4dc718f9e06e412cc1770d8da26
Daniel Fryar <daniel.fryar@gmail.com>
2020-10-15 11:21:32 -0500

    feat: try and catch blocks

--- src/exceptions/ExceptionsLesson.java
+++ src/exceptions/ExceptionsLesson.java
@@ -24,48 +24,54 @@ public class ExceptionsLesson {
         System.out.println("Start dangerZone");
 
         // dangerous code goes here
-        // ArrayIndexOutOfBoundsException
-        System.out.println("Tenth color is: " + colors[9]);
-        // NumberFormatException
-        int numHello = new Integer("hello");
-        Input input = new Input();
-        int nameInt = input.getInt("Please enter you name:");
-        System.out.println("nameInt = " + nameInt);
-        // ArithmeticException
-        int num = 1 / 0;
-        // NullPointerException
-        if (hello.equals("hello"))
-            System.out.println("Hello!");
-        // IllegalArgumentException
-        for (String color:colors)
-            System.out.printf("Hex value of %s is %s.\n",color,getColorHexValue(color));
-
+        try {
+            // ArrayIndexOutOfBoundsException
+//            System.out.println("Tenth color is: " + colors[9]);
+            // NumberFormatException
+//            int numHello = new Integer("hello");
+            Input input = new Input();
+            int nameInt = input.getInt("Please enter you name:");
+            System.out.println("nameInt = " + nameInt);
+            // ArithmeticException
+            int numerator = 1, denominator = 0;
+            int num = numerator / denominator;
+            // NullPointerException
+            if (hello.equals("hello"))
+                System.out.println("Hello!");
+            // IllegalArgumentException
+            for (String color : colors)
+                System.out.printf("Hex value of %s is %s.\n", color, getColorHexValue(color));
+        } catch (ArrayIndexOutOfBoundsException aibex) {
+            System.out.println("ArrayIndexOutOfBoundsException");
+            System.out.println(aibex.getMessage());
+        } catch (NumberFormatException nfex) {
+            System.out.println("Number Format Exception");
+            System.out.println(nfex.getMessage());
+//        } catch (Exception ex) {
+//            System.out.println(ex.getMessage());
+//            ex.printStackTrace();
+        }
         System.out.println("Finish dangerZone");
     }
 
-    static String getColorHexValue(String colorName){
+    static String getColorHexValue(String colorName) {
         switch (colorName.toLowerCase()) {
-            case "red":
-                return "110000";
-            case "green":
-                return "001100";
-            case "blue":
-                return "000011";
-            default:
-                throw new IllegalArgumentException("Unknown Color");
+            case "red": return "FF0000";
+            case "green": return "00FF00";
+            case "blue": return "0000FF";
+            default: throw new IllegalArgumentException("Unknown Color");
                 // or RuntimeException
         }
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
-        first();
+//        first();
+        if (Math.floor(Math.random()*3) == 0)
+            throw new Exception("A problem happened");
         System.out.println("Program Finished Successfully. Yay!!");
     }
 
 }
 
-
-
-
-//class BIOException extends Exception {}
+class BIOException extends Exception {}
